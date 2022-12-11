d013fb35aacd67b22b5528bec8439a049c13e572
G0dObject <vladimirgjob@gmail.com>
2022-12-11 12:00:36 +0300

    move class to relevant directory

---
+++ Notes.Application/Common/Note/CreateNote.cs
@@ -0,0 +1,8 @@
+﻿namespace Notes.Application.Common.Note
+{
+	public class CreateNote
+	{
+		public string Title { get; set; } = string.Empty;
+		public string Text { get; set; } = string.Empty;
+	}
+}
