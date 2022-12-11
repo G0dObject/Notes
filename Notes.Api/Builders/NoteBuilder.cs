56156ac3a445811745d37e266c191152ba650335
G0dObject <vladimirgjob@gmail.com>
2022-12-11 11:58:43 +0300

    Added INoteBuilder and a class that implements it

---
+++ Notes.Api/Builders/NoteBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Notes.Application.Interfaces.Builders;
+using Notes.Domain.Entity;
+using Notes.Domain.Entity.Authorization;
+
+namespace Notes.Api.Builders
+{
+	internal class NoteBuilder : INoteBuilder
+	{
+		private Note buildnote = new();
+		public INoteBuilder AddText(string text)
+		{
+			buildnote.Text = text;
+			return this;
+		}
+
+		public INoteBuilder AddTitle(string title)
+		{
+			buildnote.Title = title;
+			return this;
+		}
+
+		public INoteBuilder AddUser(User user)
+		{
+			buildnote.User = user;
+			return this;
+		}
+		public Note GetNote() => buildnote;
+
+	}
+}
---
+++ Notes.Application/Interfaces/Builders/INoteBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Notes.Domain.Entity;
+using Notes.Domain.Entity.Authorization;
+
+namespace Notes.Application.Interfaces.Builders
+{
+	public interface INoteBuilder
+	{
+		public INoteBuilder AddTitle(string title);
+		public INoteBuilder AddText(string text);
+		public INoteBuilder AddUser(User user);
+		public Note GetNote();
+	}
+}
