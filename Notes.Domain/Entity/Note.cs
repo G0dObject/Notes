8670e4f305caa7b920251a08b46cbed1730200c6
G0dObject <vladimirgjob@gmail.com>
2022-12-11 10:06:29 +0300

    Add field text to Note.cs

--- Notes.Domain/Entity/Note.cs
+++ Notes.Domain/Entity/Note.cs
@@ -6,6 +6,7 @@ namespace Notes.Domain.Entity
 	public class Note : BaseEntity
 	{
 		public string Title { get; set; } = string.Empty;
+		public string Text { get; set; } = string.Empty;
 		public int Id { get; set; }
 		public virtual User? User { get; set; }
 		public int UserId { get; set; }
