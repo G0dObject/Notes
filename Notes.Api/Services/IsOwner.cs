60e0ed6348414e6e7a356d82f24272f974eee211
G0dObject <vladimirgjob@gmail.com>
2022-12-12 12:01:31 +0300

    Added new generic services required to prove user ownership of a database object.

--- Notes.Api/Services/IsOwner.cs
+++ Notes.Api/Services/IsOwner.cs
@@ -1,42 +1,32 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Notes.Api.Builders;
-using Notes.Api.Services;
-using Notes.Application.Common.Note;
 using Notes.Application.Interfaces;
 using Notes.Domain.Base;
-using Notes.Domain.Entity;
 using Notes.Domain.Entity.Authorization;
 using Notes.Persistence;
 using System.Security.Claims;
 
 namespace Notes.Api.Services
 {
-	public class IsOwner<TUser, TObject> where TUser : IdentityUser<int> where TObject : class, BaseEntity
+	public class IsOwner<TUser, TObject> where TUser : class where TObject : class, IBaseEntityOneTo
 	{
 		private readonly UserManager<User> _userManager;
 		private readonly NotesContext _context;
 		public IsOwner(UserManager<User> userManager, INotesDbContext context)
 		{
 			_userManager = userManager;
-			_context = context as NotesContext;
+			_context = (NotesContext)context;
 		}
 		public async Task<bool> Check(ClaimsPrincipal claims, int id)
 		{
 			TObject? obj = await _context.Set<TObject>().FirstOrDefaultAsync(a => a.Id == id);
 			User? user = await _userManager.GetUserAsync(claims);
 
-			if (obj.Id == user.Id)
-			{
-				return true;
-			}
-			else
-			{
+			if (obj == null || user == null)
 				return false;
-			}
+			else if (obj.UserId == user.Id)
+				return true;
+			return false;
 		}
 	}
 }
